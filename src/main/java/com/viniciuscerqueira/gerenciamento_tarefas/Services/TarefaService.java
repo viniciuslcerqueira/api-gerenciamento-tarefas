3658c4febdf515f671e744bcae1f7d2a6e41e8b8
Vinicius Cerqueira <viniciusleitecerqueira@gmail.com>
2025-06-14 18:11:56 -0300

    Service Usuario + Service Tarefa

--- src/main/java/com/viniciuscerqueira/gerenciamento_tarefas/Services/TarefaService.java
+++ src/main/java/com/viniciuscerqueira/gerenciamento_tarefas/Services/TarefaService.java
@@ -24,6 +24,7 @@ public class TarefaService {
         return tarefaRepository.findAll();
     }
 
+
     public void deletarTarefa(Integer id){
         if(!tarefaRepository.existsById(id)){
             throw new RuntimeException("Tarefa não encontrada");
