9dc34e851e52c36dab5af3cff570e715a6157abd
Vinicius Cerqueira <viniciusleitecerqueira@gmail.com>
2025-06-14 13:39:46 -0300

    Initial commit

---
+++ src/test/java/com/viniciuscerqueira/gerenciamento_tarefas/GerenciamentoTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciuscerqueira.gerenciamento_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
